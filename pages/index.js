d8658c758f97f5f52dec80972b474dab062f8328
Nathan Hodnett <nathanhodnett@gmail.com>
2023-04-18 12:25:02 -0600

    Add Main

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
+import Main from '@/components/Main'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <title>Nathan | Front End Developer</title>
       </Head>
       <Navbar />
+      <Main />
     </div>
     
   )
