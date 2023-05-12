cbe796c9da8d63b8d04cad759010ca6517d98004
Nathan Hodnett <nathanhodnett@gmail.com>
2023-05-12 14:53:07 -0600

    Remove Skills altogether

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,149 +1,149 @@
-import React from 'react'
-import Image from 'next/image'
-import reactImg from '../public/assets/skills/react.png'
-import githubImg from '../public/assets/skills/github1.png'
-import javascriptImg from '../public/assets/skills/javascript.png'
-import typescriptImg from '../public/assets/skills/typescript.png'
-import htmlImg from '../public/assets/skills/html.png'
-import cssImg from '../public/assets/skills/css.png'
-import nextImg from '../public/assets/skills/nextjs.png'
-import nodeImg from '../public/assets/skills/node.png'
-import tailwindImg from '../public/assets/skills/tailwind.png'
-import graphqlImg from '../public/assets/skills/graphql.png'
-import vscodeImg from '../public/assets/skills/vscode.png'
-import circleciImg from '../public/assets/skills/circleci.png'
+// import React from 'react'
+// import Image from 'next/image'
+// import reactImg from '../public/assets/skills/react.png'
+// import githubImg from '../public/assets/skills/github1.png'
+// import javascriptImg from '../public/assets/skills/javascript.png'
+// import typescriptImg from '../public/assets/skills/typescript.png'
+// import htmlImg from '../public/assets/skills/html.png'
+// import cssImg from '../public/assets/skills/css.png'
+// import nextImg from '../public/assets/skills/nextjs.png'
+// import nodeImg from '../public/assets/skills/node.png'
+// import tailwindImg from '../public/assets/skills/tailwind.png'
+// import graphqlImg from '../public/assets/skills/graphql.png'
+// import vscodeImg from '../public/assets/skills/vscode.png'
+// import circleciImg from '../public/assets/skills/circleci.png'
 
-const Skills = () => {
-  return (
-    <div id='skills' className='w-full lg:h-screen p-2'>
-        <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-            <p className='text-xl tracking-widest uppercase text-[#077187]'>Skills</p>
-            <h2 className='py-4'>What I Can Do</h2>
-            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={reactImg} alt='React Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>REACT</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={githubImg} alt='GitHub Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>GITHUB</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={javascriptImg} alt='Javascript Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>JAVASCRIPT</h3>
-                        </div>
-                    </div>
-                </div>
-                    <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={typescriptImg} alt='Typescript Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>TYPESCRIPT</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={htmlImg} alt='HTML Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>HTML</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={cssImg} alt='CSS Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>CSS</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={nextImg} alt='Next Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>NEXTJS</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={nodeImg} alt='Node Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>NODE</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={tailwindImg} alt='Tailwind Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>TAILWIND</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={graphqlImg} alt='GraphQL Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>GRAPH QL</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={vscodeImg} alt='VSCode Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>VS CODE</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={circleciImg} alt='CircleCI Icon'/>
-                        </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3>CIRCLE CI</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+// const Skills = () => {
+//   return (
+//     <div id='skills' className='w-full lg:h-screen p-2'>
+//         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+//             <p className='text-xl tracking-widest uppercase text-[#077187]'>Skills</p>
+//             <h2 className='py-4'>What I Can Do</h2>
+//             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={reactImg} alt='React Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>REACT</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={githubImg} alt='GitHub Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>GITHUB</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={javascriptImg} alt='Javascript Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>JAVASCRIPT</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                     <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={typescriptImg} alt='Typescript Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>TYPESCRIPT</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={htmlImg} alt='HTML Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>HTML</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={cssImg} alt='CSS Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>CSS</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={nextImg} alt='Next Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>NEXTJS</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={nodeImg} alt='Node Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>NODE</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={tailwindImg} alt='Tailwind Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>TAILWIND</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={graphqlImg} alt='GraphQL Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>GRAPH QL</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={vscodeImg} alt='VSCode Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>VS CODE</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+//                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+//                         <div className='m-auto'>
+//                             <Image src={circleciImg} alt='CircleCI Icon'/>
+//                         </div>
+//                         <div className='flex flex-col items-center justify-center'>
+//                             <h3>CIRCLE CI</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Skills
+// export default Skills
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
       <Main />
       <About />
-      <Skills />
+      {/* <Skills /> */}
       <Projects />
       <Contact />
     </div>
